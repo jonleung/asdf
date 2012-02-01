f578621418b69e52257a283b0045393c42a36eb2
Jonathan <jonleung@seas.upenn.edu>
2012-02-01 15:50:37 -0500

    first commit

---
+++ test/edu/upenn/cis350/gpx/TestGPXcalculator.java
@@ -0,0 +1,31 @@
+package edu.upenn.cis350.gpx;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+public class TestGPXcalculator {
+
+	@BeforeClass
+	public void setUpClass() throws Exception {
+		
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
